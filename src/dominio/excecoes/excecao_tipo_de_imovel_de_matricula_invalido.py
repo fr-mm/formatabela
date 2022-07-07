a3dbc7a5144b6023d2409cbbc45bb7fc6035f564
chico <chico.zimcolor@gmail.com>
2022-07-07 15:45:51 -0300

    ExcecaoTipoDeImovelInvalido com mensagem mais clara

--- src/dominio/excecoes/excecao_tipo_de_imovel_de_matricula_invalido.py
+++ src/dominio/excecoes/excecao_tipo_de_imovel_de_matricula_invalido.py
@@ -5,7 +5,7 @@ from src.dominio.excecoes import ExcecaoDeDominio
 class ExcecaoTipoDeImovelDeMatriculaInvalido(ExcecaoDeDominio):
     def __init__(self, valor: str) -> None:
         opcoes = ''.join([f'- {tipo_de_imovel.value}\n' for tipo_de_imovel in EnumTipoDeImovel])
-        mensagem = f'Valor: {valor}' \
+        mensagem = f'Tipo de imóvel inválido: {valor}' \
                    f'\ndeve ser uma das opções:' \
                    f'\n{opcoes}'
         super().__init__(mensagem)
